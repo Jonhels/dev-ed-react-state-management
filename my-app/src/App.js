b7785342ed257e89926a78e23e486f5dab7f89bd
Jonhels <jonhels8888@gmail.com>
2020-12-15 20:56:04 +0100

    added form, learning finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MovieList from './MovieList';
 import Nav from './Nav';
+import AddMovie from './AddMovie';
 import { MovieProvider } from './MovieContext';
 
 
@@ -10,6 +11,7 @@ function App() {
     <MovieProvider>
       <div className="App">
         <Nav />
+        <AddMovie />
         <MovieList />
       </div>
     </MovieProvider>
